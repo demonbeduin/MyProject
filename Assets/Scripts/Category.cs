da9d58b589e945e5824fedfdbfb3b68399f19af4
demonbeduin <team.sad.info@gmail.com>
2019-04-21 15:49:30 +0300

    test 11

--- Assets/Scripts/Category.cs
+++ Assets/Scripts/Category.cs
@@ -4,31 +4,41 @@ using UnityEngine;
 
 public class Category : MonoBehaviour
 {
-    public Light _light;
-    public int _intensity = 1;
+    public Light Light_1;
+    public int _intensity = 0;
 
     void Start()
     {
-        _light = GetComponent<Light>();
-        _light.intensity = _intensity;
+        Light_1 = GetComponent<Light>();
+        Light_1.intensity = _intensity;
     }
-    private void OnMouseOver()
-    {
-        _light.intensity = 1;
-        print("on");
+    //private void OnMouseOver()
+    //{
+    //    Light_1.intensity = 1;
+    //    print("on");
 
-    }
-    private void OnMouseExit()
-    {
-        _light.intensity = 0;
-        print("off");
-    }
+    //}
+    //private void OnMouseExit()
+    //{
+    //    Light_1.intensity = 0;
+    //    print("off");
+    //}
     void Update()
     {
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.T))
         {
-            _light.intensity = 1;
-            print("on");
+            if (_intensity == 0)
+            {
+                Light_1.intensity = 1;
+                print("on");
+                _intensity = 1;
+            }
+            else
+            {
+                Light_1.intensity = 0;
+                print("off");
+                _intensity = 0;
+            }
         }
     }
 }
