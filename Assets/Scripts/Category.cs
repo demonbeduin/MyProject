5b30bb10aeea8ea56cbc69646bc83dc66dc2f60c
demonbeduin <team.sad.info@gmail.com>
2019-04-21 11:29:55 +0300

    test 9

---
+++ Assets/Scripts/Category.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Category : MonoBehaviour
+{
+    public Light _light;
+    public int _intensity = 1;
+
+    void Start()
+    {
+        _light = GetComponent<Light>();
+        _light.intensity = _intensity;
+    }
+    private void OnMouseOver()
+    {
+        _light.intensity = 1;
+        print("on");
+
+    }
+    private void OnMouseExit()
+    {
+        _light.intensity = 0;
+        print("off");
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            _light.intensity = 1;
+            print("on");
+        }
+    }
+}
